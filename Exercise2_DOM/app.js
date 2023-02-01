f3b6a1d490c9bac0f479b6917e4a1914ca166466
haruka <haruka.1017.ishikawa@gmail.com>
2023-02-01 08:59:17 -0800

    submit

--- Exercise2_DOM/app.js
+++ Exercise2_DOM/app.js
@@ -1,9 +1,27 @@
 //Adding a note
-const addNote = document.getElementById("add-btn");
-addNote.addEventListener('click', (e) => {
-  const newNote = document.createElement('li');
-  newNote.innerHTML = ''
+// const addNote = document.getElementById("add-btn");
+// addNote.addEventListener('click', (e) => {
+//   const newNote = document.createElement('li');
+//   newNote.innerHTML = ''
   
-  const addList = document.getElementById("list");
-  addList.appendChild("addNote");
-})
+//   const addList = document.getElementById("list");
+//   addList.appendChild("addNote");
+// })
+
+const addBtn = document.getElementById('add-btn')
+const input = document.getElementById('add-input')
+
+const addNote = (e) => {
+  let inputVal = input.value
+  e.preventDefault()
+  const list = document.getElementById('list')
+  const li = document.createElement('li')
+  li.innerHTML = `<p>${inputVal}</p>
+                  <p>
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i
+                    </p>
+                    <input type="text" class="edit-note">`
+  list.appendChild(li)
+  input.value = ''
+}
+addBtn.addEventListener('click', addNote)
