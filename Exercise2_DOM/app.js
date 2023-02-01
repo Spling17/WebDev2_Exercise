155edb79175dfee8b39fcb6b29a4f55528ffd58b
haruka <haruka.1017.ishikawa@gmail.com>
2023-02-01 08:37:52 -0800

    submit

--- Exercise2_DOM/app.js
+++ Exercise2_DOM/app.js
@@ -1,1 +1,9 @@
 //Adding a note
+const addNote = document.getElementById("add-btn");
+addNote.addEventListener('click', (e) => {
+  const newNote = document.createElement('li');
+  newNote.innerHTML = ''
+  
+  const addList = document.getElementById("list");
+  addList.appendChild("addNote");
+})
